aa8df7ae7cbf0790867732732ac6394463309ce8
Lassi Kortela <lassi@lassi.io>
2019-12-10 17:06:14 +0200

    Change c99 to gnu99 to get at POSIX definitions

--- build-strict.sh
+++ build-strict.sh
@@ -3,6 +3,6 @@ set -eu
 cd "$(dirname "$0")"
 echo "Entering directory '$PWD'"
 CC=${CC:-gcc}
-CFLAGS=${CFLAGS:--Wall -Wextra -pedantic -std=c99 -Og -g}
+CFLAGS=${CFLAGS:--Wall -Wextra -pedantic -std=gnu99 -Og -g}
 set -x
 $CC $CFLAGS -o scheme scheme.c
