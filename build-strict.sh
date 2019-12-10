cb3c26e6906fcee0792988a677124005518c36a1
Lassi Kortela <lassi@lassi.io>
2019-12-10 12:21:24 +0200

    Make build script cd to build dir
    Needed for CI

--- build-strict.sh
+++ build-strict.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+set -eu
+cd "$(dirname "$0")"
+echo "Entering directory '$PWD'"
 CC=${CC:-gcc}
 CFLAGS=${CFLAGS:--Wall -Wextra -pedantic -std=c99 -Og -g}
+set -x
 $CC $CFLAGS -o scheme scheme.c
