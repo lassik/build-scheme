2a20fe701a7f1f08ec26484e47f68c21af4a18d5
Lassi Kortela <lassi@lassi.io>
2019-12-15 01:46:44 +0200

    Enable buffer overflow detector in gcc/clang builds

--- build-strict.sh
+++ build-strict.sh
@@ -3,7 +3,7 @@ set -eu
 cd "$(dirname "$0")"
 echo "Entering directory '$PWD'"
 CC=${CC:-gcc}
-CFLAGS=${CFLAGS:--Wall -Wextra -pedantic -std=gnu99 -Og -g}
+CFLAGS=${CFLAGS:--Wall -Wextra -pedantic -std=gnu99 -fsanitize=address -Og -g}
 version="$(git describe --always --tags --dirty 2>/dev/null || true)"
 set -x
 $CC $CFLAGS -D SCHEME_VERSION=\""$version"\" -o discheme discheme.c
